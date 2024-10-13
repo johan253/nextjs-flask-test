d02a0c0805f82ad4b9ceccb0112e3e53d5eb739e
johan253 <johannjo2000@gmail.com>
2024-10-13 12:26:31 -0700

    set up working docker containers using nginx

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/python")
+@app.route("/api")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify(
+        f"Hello from API! the current time is {datetime.now()}"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5328)
